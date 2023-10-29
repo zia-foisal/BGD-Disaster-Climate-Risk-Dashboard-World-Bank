7e20368d8f9d6e283100205505896ab7cc43dc0e
Md Zia Uddin Foisal <86196909+zia-foisal@users.noreply.github.com>
2023-10-29 15:56:17 +0600

    folder structure and data prep script

---
+++ CCDR_Dashboard/www/wb_img.js
@@ -0,0 +1,3 @@
+	$( document ).ready(function() {
+  $( ".navbar .container-fluid" ).append( '<a href="https://databank.worldbank.org/source/country-climate-and-development-report-(ccdr)"><img src="worldbank_logo1.jpg" align="right"></a>' );
+});
