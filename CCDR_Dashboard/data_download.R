cdcf793ef04d73e8839777986c72af298df6e620
Md Zia Uddin Foisal <86196909+zia-foisal@users.noreply.github.com>
2023-11-05 21:17:33 +0600

    up dd

--- CCDR_Dashboard/data_download.R
+++ CCDR_Dashboard/data_download.R
@@ -4,7 +4,7 @@
 output$download_nat <- downloadHandler(
   filename = "Natural_Hazards.xlsx",
   content = function(file){
-    file.copy("www/Pakistan_Natural Hazard Exposure and Impact_Districts Tehsils.xlsx", file)
+    file.copy("www/BGD_Natural Hazard Exposure and Impact_Districts Tehsils.xlsx", file)
   }
 )
 
@@ -13,7 +13,7 @@ output$download_nat <- downloadHandler(
 output$download_dev <- downloadHandler(
   filename = "Development_outcomes.xlsx",
   content = function(file){
-    file.copy("www/pak_sub_ADM2_handover.xlsx", file)
+    file.copy("www/bgd_sub_ADM2_handover.xlsx", file)
   }
 )
 
